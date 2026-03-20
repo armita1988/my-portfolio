b3648ec3b973608f95dda434bdd89975ae2865de
armita1988 <armita.hajimani@gmail.com>
2026-03-19 22:46:52 -0400

    improved UI

--- src/components/WorkHistoryItem/WorkHistoryItem.js
+++ src/components/WorkHistoryItem/WorkHistoryItem.js
@@ -2,11 +2,13 @@ import styles from './WorkHistoryItem.module.css';
 export default function WorkHistoryItem({ workHistoryItem }) {
   return (
     <div className={styles['work-history-item']}>
-      <div className={styles['logo-box']}>
-        <img className={styles.logo} src={workHistoryItem.logo} alt={`${workHistoryItem.title}-logo`} />
-      </div>
       <div className={styles['work-history-content']}>
-        <h3 className={styles.title}>{`${workHistoryItem.title}, ${workHistoryItem.company}`}</h3>
+        <div className={styles['title-container']}>
+          <div className={styles['logo-box']}>
+            <img className={styles.logo} src={workHistoryItem.logo} alt={`${workHistoryItem.title}-logo`} />
+          </div>
+          <h3 className={styles.title}>{`${workHistoryItem.title}, ${workHistoryItem.company}`}</h3>
+        </div>
         <p className={styles.duration}>{`${workHistoryItem.startDate} - ${workHistoryItem.endDate}`}</p>
         <ul className={styles.tasks}>
           {workHistoryItem.tasks.map((task) => (
